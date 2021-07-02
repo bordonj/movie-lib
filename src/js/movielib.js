972c3bc6ae4026d98dfc9cf3242057e0af06fdb7
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-01 22:26:42 -0700

    fix logic to make async code act sync so .then works

--- src/js/movielib.js
+++ src/js/movielib.js
@@ -1,22 +1,32 @@
 var axios = require("axios").default;
 
 export default class Searches {
-  static async get(input) {
+  static async fetchData(input) {
     var options = {
       method: 'GET',
       url: 'https://imdb8.p.rapidapi.com/auto-complete',
       params: {q: `${input}`},
       headers: {
-        'x-rapidapi-key': `bf95d16b3dmsh27d4cf5aaca369bp1193f7jsn9fa26dc8141d`,
+        'x-rapidapi-key': `${process.env.API_KEY}`,
         'x-rapidapi-host': 'imdb8.p.rapidapi.com'
       }
     };
-    axios.request(options).then(function (response) {
-      console.log(response.data);
-      return response.data;
-    }).catch(function (error) {
-      console.error(error);
-      return error;
-    });
+    console.log('got to axios');
+
+    try {
+      let data = await axios.request(options);
+      console.log('axios try', data);
+      return data;
+      // -----used to be below without await! this didn't work----
+      // .then(function (response) {
+      //   console.log('axios data', response.data);
+      //   return response.data;
+      // })
+
+    } catch (err) {
+      console.error(err);
+      // return err;
+    }
+
   }
 }
--- src/main.js
+++ src/main.js
@@ -8,21 +8,32 @@ $(document).ready(function() {
   $('#userInput').submit(function(e) {
     e.preventDefault();
     let input = $('#search').val();
-    Searches.get(input)
-      .then(function(getName) {
-        console.log('getName', getName);
-        if (getName instanceof Error) {
-          throw Error('error to get name');
+    console.log('searches', Searches);
+    console.log('env', process.env.API_KEY);
+    Searches.fetchData(input)
+      .then(res => {
+        console.log('res', res);
+        if(res instanceof Error) {
+          throw Error('error to get to res');
         }
-        const dataArr = getName.d;
-        for (let actor of dataArr) {
-          console.log('actor name', actor.l);
+        let str = '';
+        console.log('res', res);
+        const dataArr = res.data.d;
+        
+        for (let element of dataArr) {
+          if (element.i) {
+            str += `<h1>${element.l}</h1><img src='${element.i.imageUrl}'><br><br>`;
+            console.log('actor name', element.l);
+            console.log('img', element.i.imageUrl);
+          } else {
+            str += `
+            <h1>${element.l}</h1>
+            <p>type: ${element.q}</p>
+            `;
+          }
         }
+        $('.output').html(str);
       })
-      .catch(function(err) {
-        console.log('in catch, err', err);
-      });
+      .catch(err => console.log('err', err));
   });
 });
-// Searches.get();
-// import { Project } from 'js/project';
